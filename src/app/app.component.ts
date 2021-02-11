773e9e4dfdef9b46cd0f60b70fccdfab3a01b8e3
Agnei Carvalho Silva <agnei.silva@confitec.com.br>
2021-02-11 11:25:16 -0300

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { StyleService } from './style.service';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit {
-
-  clientId = 0;
-
-  public isSwitcherShown = false;
-
-  constructor(private actRoute: ActivatedRoute, private styleService: StyleService) { }
-
-  toggleThemeSwitcher() {
-    this.isSwitcherShown = !this.isSwitcherShown;
-  }
-
-  ngOnInit() {
-
-    this.actRoute.queryParams.subscribe(params => {
-      this.clientId = params['client'];
-      if (this.clientId !== undefined)
-        this.styleService.setStyle(this.clientId);
-    });
-  }
-
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { ThemePickerComponent } from './components/theme-picker/theme-picker.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule, Routes } from '@angular/router';
-
-import { APP_BASE_HREF } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  }
-];
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), HttpClientModule],
-  declarations: [AppComponent, CardComponent, NavigationComponent, ThemePickerComponent, FooterComponent],
-  bootstrap: [AppComponent],
-
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
